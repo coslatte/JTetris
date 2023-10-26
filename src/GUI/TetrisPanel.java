6acabb6cb7a221af4ec6c873b8546250901c3730
cosLatte <129236813+coslatte@users.noreply.github.com>
2023-10-25 23:21:27 -0400

    Commit.

--- src/GUI/TetrisPanel.java
+++ src/GUI/TetrisPanel.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class TetrisPanel extends JPanel implements Runnable {
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
+    private static final int FPS = 60;
     Thread gameThread;
 
     public TetrisPanel() {
@@ -14,11 +15,8 @@ public class TetrisPanel extends JPanel implements Runnable {
         setLayout(null);
     }
 
-    /**
-     * Este método inicia el thread del Panel principal (TetrisPanel).
-     */
+    // Este método inicia el thread del Panel principal (TetrisPanel).
     public void startGameThread() {
-        //
         gameThread = new Thread(this);
         gameThread.start();
     }
@@ -28,20 +26,60 @@ public class TetrisPanel extends JPanel implements Runnable {
         /*
         Dentro de este while loop, vamos a necesitar 2 cosas para que el loop
         funcione correctamente:
-
-        - Actualizar.
-        - Pintar.
-
+        1. Actualizar. - update()
+        2. Pintar. - paintComponent()
         Para eso entonces se definen los métodos 'update()' y 'paintComponent()'
         que se encargarán de actualizar datos y pintar cosas en pantalla con
         esos datos actualizados y nuevos.
-         */
-        while (gameThread != null) {
-            // método para actualizar
-            update();
 
-            // método para pintar el componente
-            repaint();
+        El método update() se va a actualizar tantas veces la CPU tenga las
+        capacidades para hacerlo, dependiendo de qué tan fuerte sea. Por lo tanto,
+        tenemos que buscar un límite de repeticiones para que los bloques
+        no avancen unidades infinitas y sí lógicas.
+         */
+
+        // El game loop que vamos a usar será 'delta/accumulator loop'.
+        double drawInterval = (double) 1000000000 / FPS; // Muchos ceros, gran precisión. Esto no es más que 0.016 seg.
+        double delta = 0;
+
+        /*
+        nanoTime devuelve el tiempo de corrida actual de la Java Virtual Machine en nanosegundos.
+        1 segundo = 1.000.000 nanosegundos.
+        Esto hace falta para tener control sobre los FPS del juego.
+         */
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+
+            // Se toma el valor del tiempo actual en nanosegundos.
+            currentTime = System.nanoTime();
+
+            /*
+            Se resta el tiempo actual (currentTime) con el último tiempo registrado (lastTime), o sea, cuánto tiempo
+            ha pasado, y se divide entre los intervalos para dibujar que asignamos en 'drawInterval'. Finalmente,
+            dicho resultado se suma a la variable 'delta'.
+             */
+            delta += (currentTime - lastTime) / drawInterval;
+
+            // El tiempo actual se guarda en el último tiempo registrado.
+            lastTime = currentTime;
+
+            /*
+            Lo que pasa en este 'if statement' es que en cada iteración, agregamos (en 'delta') el tiempo transcurrido,
+            dividido por el intervalo de pintar, y cuando 'delta' llega a ser igual al intervalo de pintar que asignamos
+            entonces actualizamos -update() y pintamos -repaint(). Finalmente reiniciamos el valor de 'delta'
+            disminuyendo su valor en 1.
+             */
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+
         }
     }
 
