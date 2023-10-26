dde2de0cae805dfd61aa7ca94655bb9593c06cac
cosLatte <129236813+coslatte@users.noreply.github.com>
2023-10-25 21:11:15 -0400

    Renamed.

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -2,15 +2,18 @@ package GUI;
 
 import javax.swing.*;
 
-public class TMain {
+public class MainGUI {
     public static void main(String[] args) {
 
         JFrame mainFrame = new JFrame("JTetris test application");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setResizable(false);
+
+        TetrisPanel tp = new TetrisPanel();
+        mainFrame.add(tp);
+        mainFrame.pack();
+
         mainFrame.setLocationRelativeTo(null);
-
-
         mainFrame.setVisible(true);
     }
 }
