ad78b0fcddf33d5f4963dc8301ed1f81896840ac
cosLatte <129236813+coslatte@users.noreply.github.com>
2023-10-25 20:20:41 -0400

    Added packages GUI and Blocks for now.

--- src/GUI/TMain.java
+++ src/GUI/TMain.java
@@ -1,7 +1,16 @@
 package GUI;
 
+import javax.swing.*;
+
 public class TMain {
     public static void main(String[] args) {
 
+        JFrame mainFrame = new JFrame("JTetris test application");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
+        mainFrame.setLocationRelativeTo(null);
+
+
+        mainFrame.setVisible(true);
     }
 }
