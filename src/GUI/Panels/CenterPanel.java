379766e0e354aa1661e58a6fe0f3fb16fb846fd2
cosLatte <129236813+coslatte@users.noreply.github.com>
2023-10-28 14:39:17 -0400

    Hice un cambio de ejemplo.

--- src/GUI/Panels/CenterPanel.java
+++ src/GUI/Panels/CenterPanel.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class CenterPanel extends JPanel {
     public CenterPanel() {
         setPreferredSize(new Dimension(TetrisPanel.WIDTH / 3, TetrisPanel.HEIGHT));
-        setLocation(TetrisPanel.WIDTH / 3, 0);
+        setLocation(TetrisPanel.WIDTH / 3, 0); // comentario
         setBackground(Color.decode("#303030"));
         setLayout(null);
     }
